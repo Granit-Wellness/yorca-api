33300826e392697166c328366b7c8f2fb605abf8
David Noah <davidnoah@clearbit.com>
2020-04-05 19:00:37 -0700

    More drug columns

--- app/models/drug.rb
+++ app/models/drug.rb
@@ -1,8 +1,6 @@
 module Yorca
   module Models
-    class Drug < Sequel::Model(:drugs)
-      one_to_many :posts
-  
+    class Drug < Sequel::Model(:drugs)  
       dataset_module do
         def find_or_create!(values)
           self[values] || Drug.create(values)
@@ -21,6 +19,9 @@ end
 #  external_resource_url | text                        |
 #  created_at            | timestamp without time zone |
 #  updated_at            | timestamp without time zone |
+#  aliases               | jsonb                       | DEFAULT '[]'::jsonb
+#  health_risks          | jsonb                       | DEFAULT '[]'::jsonb
+#  effects               | jsonb                       | DEFAULT '[]'::jsonb
 # Indexes:
 #  drugs_pkey     | PRIMARY KEY btree (id)
 #  drugs_name_key | UNIQUE btree (name)
--- scripts/seed_drugs.rb
+++ scripts/seed_drugs.rb
@@ -17,11 +17,14 @@ drugs.each do |drug|
   puts "Creating #{drug["fields"]["name"]}...."
 
   drug_fields = drug["fields"]
-  Models::Drug.find_or_create!(
+  Models::Drug.create(
     name: drug_fields["name"],
     description: drug_fields["description"],
     avatar_uri: drug_fields["image"],
-    external_resource_url: drug_fields["additional_media"]
+    external_resource_url: drug_fields["additional_media"],
+    aliases: drug_fields['aliases'].to_a,
+    effects: drug_fields['effects'].to_a,
+    health_risks: drug_fields['health_risks'].to_a,
   )
   puts '..'
 end
